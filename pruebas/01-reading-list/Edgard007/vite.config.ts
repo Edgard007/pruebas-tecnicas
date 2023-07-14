6f5ac528aba579035c6e9047a0e2f2da6351fa90
Edgardo Palacios <edgard07hp@gmail.com>
2023-07-14 10:17:50 -0600

    🔧 Alias path

--- pruebas/01-reading-list/Edgard007/vite.config.ts
+++ pruebas/01-reading-list/Edgard007/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@src": "/src",
+      "@assets": "/src/assets",
+      "@common": "/src/common",
+      "@components": "/src/components",
+      "@config": "/src/config",
+      "@helpers": "/src/helpers",
+      "@pages": "/src/pages",
+      "@utils": "/src/utils",
+      "@hooks": "/src/hooks",
+      "@store": "/src/store",
+      "@actions": "/src/store/actions",
+      "@interfaces": "/src/store/interfaces",
+      "@reducers": "/src/store/reducers",
+    },
+  },
+});
