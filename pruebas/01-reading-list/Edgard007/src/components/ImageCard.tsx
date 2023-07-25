cfe2f15a7d2c257d0610e7fd09ffb89c05f826cd
Edgardo Palacios <edgard07hp@gmail.com>
2023-07-24 23:55:16 -0600

    🚧 Image card component

--- pruebas/01-reading-list/Edgard007/src/components/ImageCard.tsx
+++ pruebas/01-reading-list/Edgard007/src/components/ImageCard.tsx
@@ -1,9 +1,23 @@
-const ImageCard = ({ image }: { image: Image }) => {
+import styled from "styled-components";
+
+interface Props {
+  url: string;
+}
+
+const ImageCard = ({ url }: Props) => {
   return (
-    <div>
-      <img src={image.webformatURL} alt="" />
-    </div>
+    <Wrapper>
+      <img src={url} alt="img" />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 export default ImageCard;
--- pruebas/01-reading-list/Edgard007/src/pages/Home.tsx
+++ pruebas/01-reading-list/Edgard007/src/pages/Home.tsx
@@ -1,9 +1,45 @@
+import styled from "styled-components";
+
+// ==> Context
+import useLibraryContext from "@contexts/useLibraryContext";
+
+// ==> Components
+import ImageCard from "@components/ImageCard";
+
 const Home = () => {
+  const {
+    state: { library },
+  } = useLibraryContext();
+
   return (
-    <div>
+    <Wrapper>
       <h1>Home</h1>
-    </div>
+      {(library || []).map(({ book }, i) => (
+        <ImageCard key={book?.ISBN} url={book.cover} />
+      ))}
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-primary);
+
+
+  margin: 40px 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(275px, 1fr));
+  grid-gap: 30px;
+
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+`;
 
 export default Home;
